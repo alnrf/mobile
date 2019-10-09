d715d4292e478e2a588e39c28d8874a9a4380d4b
Vincent Catillon <contact@vincent-catillon.fr>
2019-10-09 15:34:09 +0200

    Fetch recommendation with language and other params

--- src/layer/data/recommendations.js
+++ src/layer/data/recommendations.js
@@ -1,13 +1,16 @@
-// @flow
+// @flow strict
 
 import decode from 'jwt-decode';
+
 import fetch from '../../modules/fetch';
 import {get as getToken} from '../../utils/local-token';
 import type {JWT} from '../../types';
+import translations from '../../translations';
 import type {DisciplineCard, ChapterCard} from './_types';
 
 // @todo replace fetchRecommendation() by find(type: string, ref: string)
 const fetchRecommendation = async (): Promise<DisciplineCard | ChapterCard | void> => {
+  const language = translations.getLanguage();
   const token = await getToken();
 
   if (!token) {
@@ -15,11 +18,14 @@ const fetchRecommendation = async (): Promise<DisciplineCard | ChapterCard | voi
   }
 
   const jwt: JWT = decode(token);
-  const response = await fetch(`${jwt.host}/api/v2/recommendations`, {
-    headers: {
-      authorization: token
+  const response = await fetch(
+    `${jwt.host}/api/v2/recommendations?contentType=all&withoutAdaptive=true&lang=${language}`,
+    {
+      headers: {
+        authorization: token
+      }
     }
-  });
+  );
 
   const {hits}: {hits: Array<DisciplineCard | ChapterCard>} = await response.json();
   return hits[0];
