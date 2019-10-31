055fe8ed5528bba11da08b69d2ba9d9cc335a180
Vincent Catillon <contact@vincent-catillon.fr>
2019-10-31 10:36:59 +0100

    Update files for 0.61.3

--- android/app/src/main/java/com/coorpacademy/app/MainApplication.java
+++ android/app/src/main/java/com/coorpacademy/app/MainApplication.java
@@ -3,7 +3,8 @@ package com.coorpacademy.app;
 import android.support.multidex.MultiDexApplication;
 
 import android.app.Application;
-
+import android.content.Context;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 import com.facebook.react.modules.email.EmailPackage;
@@ -22,57 +23,65 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
 import io.invertase.firebase.links.RNFirebaseLinksPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
-
-import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+  private final ReactNativeHost mReactNativeHost =
+    new ReactNativeHost(this) {
+      @Override
+      public boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+      }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNReactNativeHapticFeedbackPackage(),
-            new EmailPackage(),
-            new RNSoundPackage(),
-            new RNCWebViewPackage(),
-            new AsyncStoragePackage(),
-            new RNCameraPackage(),
-            new ReactVideoPackage(),
-            new SvgPackage(),
-            new OrientationPackage(),
-            new ReactNativeLocalizationPackage(),
-            new LinearGradientPackage(),
-            new RNFetchBlobPackage(),
-            new RCTPdfView(),
-            new RNGestureHandlerPackage(),
-            new RNDeviceInfo(),
-            new RNFirebasePackage(),
-            new RNFirebaseAnalyticsPackage(),
-            new RNFirebaseLinksPackage(),
-            new SplashScreenReactPackage(),
-            new LottiePackage()
-      );
-    }
+      /*@Override
+      protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+              new RNReactNativeHapticFeedbackPackage(),
+              new EmailPackage(),
+              new RNSoundPackage(),
+              new RNCWebViewPackage(),
+              new AsyncStoragePackage(),
+              new RNCameraPackage(),
+              new ReactVideoPackage(),
+              new SvgPackage(),
+              new OrientationPackage(),
+              new ReactNativeLocalizationPackage(),
+              new LinearGradientPackage(),
+              new RNFetchBlobPackage(),
+              new RCTPdfView(),
+              new RNGestureHandlerPackage(),
+              new RNDeviceInfo(),
+              new RNFirebasePackage(),
+              new RNFirebaseAnalyticsPackage(),
+              new RNFirebaseLinksPackage(),
+              new SplashScreenReactPackage(),
+              new LottiePackage()
+        );
+      }*/
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+      @Override
+      protected List<ReactPackage> getPackages() {
+        @SuppressWarnings("UnnecessaryLocalVariable")
+        List<ReactPackage> packages = new PackageList(this).getPackages();
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // packages.add(new MyReactNativePackage());
+        return packages;
+      }
+
+      @Override
+      protected String getJSMainModuleName() {
+        return "index";
+      }
+    };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -83,5 +92,32 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+  }
+
+  /**
+   * Loads Flipper in React Native templates.
+   *
+   * @param context
+   */
+  private static void initializeFlipper(Context context) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
