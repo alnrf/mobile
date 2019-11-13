4e335f64cd0996a879da1e5e10ea8ef5e5ea7330
Vincent Catillon <contact@vincent-catillon.fr>
2019-11-13 19:03:50 +0100

    Move modal styles into modal component

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,17 +1,85 @@
 // @flow
 
 import * as React from 'react';
+import {StyleSheet, View} from 'react-native';
 import ModalBase from 'react-native-modal';
 
+import theme from '../modules/theme';
+import HeaderBackButton from './header-back-button';
+
 export type Props = {|
-  children: React.Node,
   isVisible?: boolean,
-  onClose?: () => void
+  children: React.Node,
+  headerBackgroundColor?: string,
+  iconBackgroundColor?: string,
+  renderIcon?: () => React.Node,
+  onClose: () => void
 |};
 
-const Modal = ({isVisible = false, onClose, children}: Props) => (
-  <ModalBase isVisible={isVisible} onSwipeComplete={onClose} onBackdropPress={onClose}>
-    {children}
+const HEADER_HEIGHT = 75;
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: theme.radius.card,
+    overflow: 'hidden',
+    backgroundColor: theme.colors.white
+  },
+  header: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingHorizontal: theme.spacing.medium,
+    height: HEADER_HEIGHT
+  },
+  content: {
+    padding: theme.spacing.medium,
+    alignItems: 'center'
+  },
+  contentWithIcon: {
+    paddingTop: theme.spacing.small
+  },
+  icon: {
+    marginTop: -HEADER_HEIGHT / 2,
+    padding: 5,
+    backgroundColor: theme.colors.white,
+    borderRadius: theme.radius.thumbnail,
+    alignSelf: 'center'
+  },
+  iconContent: {
+    padding: theme.spacing.small,
+    borderRadius: theme.radius.thumbnail,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+const Modal = ({
+  isVisible,
+  children,
+  headerBackgroundColor = theme.colors.white,
+  iconBackgroundColor = theme.colors.white,
+  renderIcon,
+  onClose
+}: Props) => (
+  <ModalBase isVisible={Boolean(isVisible)} onSwipeComplete={onClose} onBackdropPress={onClose}>
+    <View style={styles.container}>
+      <View style={[styles.header, {backgroundColor: headerBackgroundColor}]}>
+        <HeaderBackButton
+          color={theme.colors.gray.dark}
+          isFloating={false}
+          testID="close-modal"
+          onPress={onClose}
+          type="close"
+        />
+      </View>
+      {renderIcon && (
+        <View style={styles.icon}>
+          <View style={[styles.iconContent, {backgroundColor: iconBackgroundColor}]}>
+            {renderIcon()}
+          </View>
+        </View>
+      )}
+      <View style={[styles.content, renderIcon && styles.contentWithIcon]}>{children}</View>
+    </View>
   </ModalBase>
 );
 
--- src/components/modal.stories.js
+++ src/components/modal.stories.js
@@ -2,12 +2,62 @@
 
 import * as React from 'react';
 import {storiesOf} from '@storybook/react-native';
+import {NovaSolidSpaceRingPlanet as RingPlanet} from '@coorpacademy/nova-icons';
 
+import {handleFakePress} from '../utils/tests';
 import Modal from './modal';
+import type {Props} from './modal';
 import Text from './text';
 
-const children = <Text>Something to display</Text>;
+type State = {|
+  isVisible: boolean
+|};
+
+// This thing is to be able to close the modal in storybook
+class ModalTester extends React.PureComponent<Props, State> {
+  props: Props;
+
+  state: State = {
+    isVisible: false
+  };
+
+  componentDidMount() {
+    this.reset(this.props.isVisible);
+  }
+
+  reset = (isVisible?: boolean = false) =>
+    this.setState({
+      isVisible
+    });
+
+  handleClose = () => this.reset();
+
+  render() {
+    return <Modal {...this.props} onClose={this.handleClose} isVisible={this.state.isVisible} />;
+  }
+}
+
+const renderIcon = () => <RingPlanet color="#f00" height={60} width={60} />;
 
 storiesOf('Modal', module)
-  .add('Default', () => <Modal>{children}</Modal>)
-  .add('Visible', () => <Modal isVisible>{children}</Modal>);
+  .add('Default', () => (
+    <ModalTester onClose={handleFakePress}>
+      <Text>Hidden modal</Text>
+    </ModalTester>
+  ))
+  .add('Visible', () => (
+    <ModalTester isVisible onClose={handleFakePress}>
+      <Text>Visible modal</Text>
+    </ModalTester>
+  ))
+  .add('Custom header and icon background color', () => (
+    <ModalTester
+      isVisible
+      renderIcon={renderIcon}
+      headerBackgroundColor="rgba(0, 128, 0, 0.2)"
+      iconBackgroundColor="rgba(0, 128, 0, 0.2)"
+      onClose={handleFakePress}
+    >
+      <Text>Modal with header and icon</Text>
+    </ModalTester>
+  ));
