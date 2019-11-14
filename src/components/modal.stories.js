f662218ffead61deeeb2997062eb69494c4d520b
Vincent Catillon <contact@vincent-catillon.fr>
2019-11-14 15:07:12 +0100

    Add unit test on modal onClose callback

--- src/components/modal.stories.js
+++ src/components/modal.stories.js
@@ -1,9 +1,11 @@
 // @flow
 
 import * as React from 'react';
+import renderer from 'react-test-renderer';
 import {storiesOf} from '@storybook/react-native';
 import {NovaSolidSpaceRingPlanet as RingPlanet} from '@coorpacademy/nova-icons';
 
+import {__TEST__} from '../modules/environment';
 import {handleFakePress} from '../utils/tests';
 import Modal from './modal';
 import Text from './text';
@@ -26,3 +28,20 @@ storiesOf('Modal', module)
       <Text>Modal with header and icon</Text>
     </Modal>
   ));
+
+if (__TEST__) {
+  describe('Modal', () => {
+    it('should handle close', () => {
+      const handleClose = jest.fn();
+      const component = renderer.create(
+        <Modal onClose={handleClose}>
+          <Text>Some content</Text>
+        </Modal>
+      );
+
+      const button = component.root.find(el => el.props.testID === 'close-modal');
+      button.props.onPress();
+      expect(handleClose).toHaveBeenCalledTimes(1);
+    });
+  });
+}
